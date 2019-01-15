49aacaf4e4360af327cad9f421d41b11bd3ab255
chivequiachon <chivequiachon@gmail.com>
2019-01-15 22:59:22 +0800

    Debug False

--- NogiOshi/settings.py
+++ NogiOshi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j7vgu!usbj)ow-0kl!o^x0sq%a*%_b(1!fzf!s+1p)cxvuj%ma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
