32369de6a00cde60845c147a54ea60971f702cfe
chivequiachon <chivequiachon@gmail.com>
2019-01-15 23:38:08 +0800

    Fixed static path

--- NogiOshi/settings.py
+++ NogiOshi/settings.py
@@ -136,11 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-print(STATIC_ROOT)
 STATIC_URL = '/static/'
-print(STATIC_URL)
-print(os.path.join(BASE_DIR, 'static'))
 
 STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, 'static'),
