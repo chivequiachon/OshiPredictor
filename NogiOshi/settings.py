026fe85ad21345f54d73c218b21a39b99ce10181
chivequiachon <chivequiachon@gmail.com>
2019-01-15 22:50:30 +0800

    Modifying static directory locations

--- NogiOshi/settings.py
+++ NogiOshi/settings.py
@@ -136,9 +136,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(_file_))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static'),
-    './static/',
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
 
 django_heroku.settings(locals())
